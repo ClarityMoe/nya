6cef2cb48bd654047d7fa9a09059e22c150e00d8
noud02 <noud.kerver@hotmail.nl>
2017-07-12 15:14:05 +0000

    fix port change thing

--- src/app.js
+++ src/app.js
@@ -5,16 +5,15 @@
  */
 
 // override to native promise
-global.Promise = require("bluebird");
+global.Promise = require('bluebird');
 
-const port = 5000;
-const favicon = require("serve-favicon");
-const express = require("express");
-const http = require("http");
+//const favicon = require("serve-favicon");
+const express = require('express');
+const http = require('http');
 const path = require('path');
-const cluster = require("cluster");
+const cluster = require('cluster');
 const os = require('os');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const numCPUs = os.cpus().length;
 
 if (cluster.isMaster) {
@@ -29,40 +28,44 @@ if (cluster.isMaster) {
 } else {
     console.log(`Worker ${process.pid} started`);
 
+    let port = 5000;
     const app = express();
     const server = http.createServer(app);
 
-    server.on('listening', () => console.log(`Started listening on port ${server.address().port}`));
+    server.on('listening', () => console.log(`Started listening on port ${port}`));
 
     server.on('error', (err) => {
-        if (err.syscall !== 'listen') return console.error(err);
+
+        if (err.syscall === 'listen') return console.error(err);
 
         switch (err.code) {
-            case 'EACCES': {
-                console.error(`Port ${server.address().port} is blocked on your network :<`);
-                server.close();
-                app.set('port', server.address().port + 1);
-                server.listen(server.address().port + 1, "0.0.0.0");
-                break;
-            }
-            case 'EADDRINUSE': {
-                console.log(`Something is already using port ${port} :<`);
-                server.close();
-                app.set('port', server.address().port + 1);
-                server.listen(server.address().port + 1, "0.0.0.0");
-                break;
-            }
-            default: {
-                console.error(err);
-                break;
-            }
+        case 'EACCES': {
+            console.error(`Port ${port} is blocked on your network :<`);
+            server.close();
+            port++;
+            app.set('port', port);
+            server.listen(port, '0.0.0.0');
+            break;
+        }
+        case 'EADDRINUSE': {
+            console.log(`Something is already using port ${port} :<`);
+            server.close();
+            port++;
+            app.set('port', port );
+            server.listen(port, '0.0.0.0');
+            break;
+        }
+        default: {
+            console.error(err);
+            break;
+        }
         }
     });
 
     app.set('port', port);
     app.set('view engine', 'pug');
     app.set('views', path.join(__dirname, 'views'));
- 
+
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(bodyParser.json());
     //app.use(favicon(path.join(__dirname, 'public', 'img', 'favicon.png'))); 
