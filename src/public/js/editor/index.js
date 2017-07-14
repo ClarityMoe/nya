460cd933b3f17d9d2083e7505d5fd41727fa1674
noud02 <noud.kerver@hotmail.nl>
2017-07-14 19:32:01 +0200

    delet this

--- src/public/js/editor/index.js
+++ src/public/js/editor/index.js
@@ -6,29 +6,3 @@
 
 var app = angular.module('awauApp', ['ngMaterial', 'ngMdIcons']);
 var ws = new WSConnection("test");
-
-/**
- * Sends something to the websocket
- * 
- * @param {Object} j JSON Object to send
- */
-
-function sendWS(j) {
-    if (ws.readyState === WebSocket.OPEN) {
-        ws.send(JSON.stringify(j, null, 4))
-    } else {
-        throw new Error('WebSocket isn\'t open');
-    }
-}
-
-setInterval(function() {
-    
-})
-
-ws.onopen = function(e) {
-    sendWS({
-        msg: 'nya'
-    });
-};
-
-ws.on
