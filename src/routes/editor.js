1a08654af369cecf275ce4bee8c5b646e3bfacd4
sr229 <aguilarralph13@gmail.com>
2017-07-14 18:03:11 +0800

    delet submodjule
    theia is gey

--- src/routes/editor.js
+++ src/routes/editor.js
@@ -1,42 +1,4 @@
 /**
- * @file editor.js
- * @description Handles the routing for IDE iframe/app.
- * @author TypeFox
+ * @file nya
+ * @description Man it's a hot one
  */
-
-require('reflect-metadata');
-const path = require('path');
-const express = require('express');
-const router = express.Router();
-const { Container } = require('inversify');
-
-const { BackendApplication, backendApplicationModule, loggerBackendModule } = require('theia-core/lib/application/node');
-const { messagingBackendModule } = require('theia-core/lib/messaging/node');
-
-const container = new Container();
-container.load(backendApplicationModule);
-container.load(messagingBackendModule);
-container.load(loggerBackendModule);
-
-const load = (raw) => Promise.resolve(raw.default).then(module => container.load(module));
-
-const start = () => {
-    const application = container.get(BackendApplication);
-    application.use(express.static(path.join(__dirname, '../../lib'), {
-        index: 'index.html'
-    }));
-    application.start();
-};
-
-Promise.resolve()
-    .then(() => Promise.resolve(require('theia-core/lib/filesystem/node/filesystem-backend-module')).then(load))
-    .then(() => Promise.resolve(require('theia-core/lib/workspace/node/workspace-backend-module')).then(load))
-    .then(() => Promise.resolve(require('theia-core/lib/preferences/node/preference-backend-module')).then(load))
-    .then(() => Promise.resolve(require('theia-core/lib/terminal/node/terminal-backend-module')).then(load))
-    .then(() => Promise.resolve(require('theia-core/lib/languages/node/languages-backend-module')).then(load))
-    .then(() => Promise.resolve(require('theia-core/lib/java/node/java-backend-module')).then(load))
-    .then(() => Promise.resolve(require('theia-core/lib/python/node/python-backend-module')).then(load))
-    .then(() => Promise.resolve(require('theia-core/lib/cpp/node/cpp-backend-module')).then(load))
-    .then(start);
-
-module.exports = router;
