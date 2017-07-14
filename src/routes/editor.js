b4c9c677f23aa500577dc935082cd16a58fdbba4
sr229 <aguilarralph13@gmail.com>
2017-07-14 18:20:55 +0800

    use native monaco

--- src/routes/editor.js
+++ src/routes/editor.js
@@ -2,3 +2,11 @@
  * @file nya
  * @description Man it's a hot one
  */
+
+ const router = require('express').Router();
+
+ router.get('/editor', (req, res, next) => {
+     res.render('/', {
+         title: 'Nya | Editor'
+     });
+ });
